3874bc594f5e19b477199ece64e95ea225522e30
Reza <rz.abs.shi@gmail.com>
2019-01-18 22:03:01 +0330

    Main file implemented

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,26 @@
 from src.Peer import Peer
+import argparse
+from ipaddress import ip_address
 
 if __name__ == "__main__":
-    server = Peer("insert IP Address", "Insert Port as Int", is_root=True)
-    server.run()
+    parser = argparse.ArgumentParser(description='Create a peer')
+    parser.add_argument('ip', help='ip of this peer', type=ip_address)
+    parser.add_argument('port', help='port of this peer', type=int)
+    
+    parser.add_argument('--root', help='this peer is a root', dest='is_root', default=False, type=bool)
+    
+    parser.add_argument('--root-ip', help='ip of root peer', type=ip_address, dest='root_ip')
+    parser.add_argument('--root-port', help='port of root peer', type=int, dest='root_port')
+    
+    args = parser.parse_args()
 
-    client = Peer("Insert IP Address", "Insert Port as Int", is_root=False,
-                  root_address=("Insert IP Address", "Insert Port as Int"))
+    if args.is_root:
+        peer = Peer(args.ip, args.port, is_root=True)
+    else:
+        if args.root_port is None or args.root_ip is None:
+            print("Error: you should specify root-ip and root-port")
+            exit(1)
+
+        peer = Peer(args.ip, args.port, is_root=None, root_address=(args.root_ip, args.root_port))
+
+    peer.run()
