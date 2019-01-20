71516c8c49b179d7b84062ae5c692089006004d3
Reza <rz.abs.shi@gmail.com>
2019-01-20 23:20:49 +0330

    minor refactoring

--- peer_root.py
+++ peer_root.py
@@ -11,17 +11,13 @@ class PeerRoot(Peer):
         return True
 
     def __handle_register_packet(self, packet: Packet):
-        _type = packet.get_body()[0]
+        _type = packet.get_body()[0:3]
 
         if _type == Packet.REQUEST:
             sender_address = packet.get_source_server_address()
-            node = self.__check_source_registered(sender_address)
-
-            if not node:
-                node = self.stream.add_node(sender_address, set_register_connection=True)
-
             packet = PacketFactory.new_register_packet(Packet.RESPONSE, self.address)
-            node.add_message_to_out_buff(packet.get_buf())
+
+            self.stream.get_or_create_node_to_server(sender_address, True).add_message_to_out_buff(packet.get_buf())
 
         elif _type == Packet.RESPONSE:
             if self.is_root:
